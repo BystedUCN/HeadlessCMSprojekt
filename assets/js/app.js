b24105af28abbcde9aa2c4e0923e2c41c75f0dfc
BystedUCN <10632179@ucn.dk>
2025-04-22 14:53:39 +0200

    Fikset forside og fjernet alt overflødigt ift console.log og udkommenteringer

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,6 @@ async function init(){
         renderRecipes(recepies); //vi sender recepires ned i funktionen renderRecipes, så den looper igennem og vi kalder funktionen her.
     } catch(err){
         console.log('err:', err);
-        resultEl.innerHTML = "Der gik noget galt - Øv. Det er vi kede af:(";
     }
 }
 
@@ -46,17 +45,9 @@ async function getToken(){
         return autoResponse.token;
     } catch(err){
         console.log('err:', err)
-        resultEl.innerHTML = "Der gik noget galt med at hente en token..";
-
     }
 }
 
-//henter vores domæne og posts med fetch api. await virker kun fordi vi har skrevet det er en async funktion.
-// async function getPublicRecipes(){
-//     const res = await fetch(domain + postsEndpoint); //dette er underforstået en GET request
-//     const recipes = await res.json(); //recepies her gemmer vi data vi hentet.
-//     return recipes;
-// }
 
 //henter private posts, ekstra sikkerhed
 async function getPrivateRecipes(token){
@@ -70,49 +61,6 @@ async function getPrivateRecipes(token){
     const recipes = await res.json(); //recepies her gemmer vi data vi hentet i variablen recipes.
     return recipes;
 }
-
-//OBS!!!!!!!!!!!!!!!!!!!
-//Vi bruger da ikke denne funktion nogen steder gør vi????????????????
-// Vi kan slette denne når vi indsat alt det med key.trim() i opskrift.js
-//hvis det er sandt at det er et array af opksrifter, looper den igennem det.
-// DOM hooks
-// const resultEl = document.querySelector(".result");
-// if(resultEl)
-// function renderRecipes(data) {
-// resultEl.innerHTML = ''; //sørger for at ved ny søgning slettes "indhold" i DOM. ellers bliver der bare ved med at blive tilføjet opskrifter igen og igen efter hver filtering.
-
-//     if (Array.isArray(data)) {
-//         data.forEach(recipe => {
-//             console.log('recipe:', recipe)
-//             const steps = recipe.acf.fremgangsmade;
-//             const stepsList = `<ol>${
-//                 Object.values(steps)
-//                     .filter(([key, step]) => key && key.trim() !== "" && step)
-//                     .map(step => `<li>${step}</li>`)
-//                     .join('')
-//             }</ol>`;
-
-//             const ingredients = recipe.acf.ingredienser;
-//             const ingredientsList = `<ul>${ Object.values(ingredients).filter(([key, ingredient]) => key && key.trim() !== "" && ingredient).map(ingredient => `<li>${ingredient}</li>`).join('')}</ul>`;
-
-
-//             resultEl.innerHTML += `
-//             <article>
-//             <img src="${recipe.acf.billede.url}" alt="Et billede af en Gateau Marcel">
-//                 <h2>${recipe.acf.titel}</h2>
-//                 <span>Varighed: ${recipe.acf.total_tid.name}</span>
-//                 ${stepsList}${ingredientsList}
-//             </article>
-//             `;
-//         })
-//     } else {
-//         resultEl.innerHTML += `
-//             <article>
-//                 <h2>${data.title.rendered}</h2>
-//                 ${data.content.rendered}
-//             </article>`
-//     }
-// }
 
 
 //FILTER
--- assets/js/forside.js
+++ assets/js/forside.js
@@ -1,50 +1,135 @@
     
     //henter opskrift med slug
-    async function getRecipeById(token, theSlug) {
+    async function getRecipeById(token) {
         //vi definere options og beder om at hente headers med vores token/kode
         const options = {
             headers:{
                 Authorization: "Bearer " + token
             }
         }
-        //vi venter på at hente vores data. domain og postendpoints er vores url og ?status=private&total_tid= er vores query parameters hvor vi anmoder om at få private post med total_tid og vi henter inkluder opskriftens slug.
-        const res = await fetch(domain + postsEndpoint + "?status=private&total_tid=" + theSlug + getRealImageUrls, options);
-        // vi gemme vores data i variabel recipe og får det i JSON
+        //vi venter på at hente vores data. domain og postendpoints er vores url
+        const res = await fetch(domain + postsEndpoint + "?status=private&per_page=100" + getRealImageUrls, options);
+        // vi gemmer vores data i variabel recipe og får det i JSON
         const recipe = await res.json();
-        // vi kalder funktionen i linje 20
-        renderRecipesById(recipe);
     }
-
-//filtere igennem de opskrifter der har den rigtige slug værdi. 
+ 
     const hurtigAftensMadSlider = document.querySelector(".hurtigAftensMadSlider");
-function renderRecipesById(recipe) {
-      console.log('recipe:', recipe)
-      //hvis det er et array vil if være true og der loopes igennem opskrifterne med forEach. Inden filteres der på om opskriften indholder total_tid med slug = "15" defineret forneden. Men den kan anvendes på de slugs man nu ønsker.
-      if (Array.isArray(recipe))
-        recipe.filter(recipe => recipe.acf.total_tid.slug === theSlug).forEach(recipe => {
+    function renderHurtigMad(recipe) {
+      const hurtigMadContainer = document.createElement("div");
+      hurtigMadContainer.classList.add("hurtigMadContainer");
+      //hvis det er et array vil if være true og der loopes igennem opskrifterne med forEach. Inden filteres der på om opskriften indholder total_tid med slug = "15" defineret forneden.
+        if (Array.isArray(recipe))
+            recipe.filter(recipe => recipe.acf.total_tid.slug === slugHurtigMad).forEach(recipe => {
+                console.log('recipe:', recipe);
+                const link = document.createElement("a");
+                link.classList.add("sliderItem");
+                link.href = `./opskrift.html?slug=${recipe.slug}`;
+            
+                const imgEl = document.createElement("img");
+                imgEl.classList.add("billedeISlider");
+                imgEl.src = recipe.acf.billede.url;
+                imgEl.alt = recipe.acf.billede.alt;
 
-            console.log('recipe:', recipe)
-            //indsætter i DOM med innerHTML og Indsætter ${recipe.slug} her også, så når man klikker på billeder bliver slug husket og den åbner den rigtige opskrift på opskriftssiden
-            hurtigAftensMadSlider.innerHTML += `
-                <a href="./opskrift.html?slug=${recipe.slug}">
-                <img class="billedISlider" src="${recipe.acf.billede.url}" alt="">
-                </a>
-            `;
-        })
+                const title = document.createElement("span");
+                title.classList.add("recipeTitle");
+                title.textContent = recipe.title.rendered;
+
+                link.appendChild(imgEl);
+                link.appendChild(title);
+                hurtigMadContainer.appendChild(link);
+        });
+        hurtigAftensMadSlider.appendChild(hurtigMadContainer);
+    };
     
+    const gaesteMadSlider = document.querySelector(".gaesteMadSlider")
+    function renderGaesteMad(recipe) {
+        console.log('recipe:', recipe);
+        const gaesteMadContainer = document.createElement("div");
+        gaesteMadContainer.classList.add("gaesteMadContainer");
+
+        if (Array.isArray(recipe)) {
+            recipe.filter(recipe => 
+                Array.isArray(recipe.acf.hovedkategori) // Her tjekker den om der er et term_id, som matcher vores slug 
+                && recipe.acf.hovedkategori.some(kat => kat.term_id == slugGaesteMad)).forEach(recipe => { // her tjekker vi om der er et term_id som matcher vores slug
+
+            const link = document.createElement("a");
+            link.classList.add("sliderItem");
+            link.href = `./opskrift.html?slug=${recipe.slug}`;
+
+            const imgEl = document.createElement("img");
+            imgEl.classList.add("billedeISlider");
+            imgEl.src = recipe.acf.billede.url;
+            imgEl.alt = recipe.acf.billede.alt;
+
+            const title = document.createElement("span");
+            title.classList.add("recipeTitle");
+            title.textContent = recipe.title.rendered;
+
+            link.appendChild(imgEl);
+            link.appendChild(title);
+            gaesteMadContainer.appendChild(link);
+        });
     }
-    
 
-    //gemmer varibalen theSLug og sætter den til være = 15, da det er vores taxonmi med 15-30 min som vi øsnker at hente.
-    const theSlug = "15"; 
+  gaesteMadSlider.appendChild(gaesteMadContainer);
+}
+
+    const weekendMadSlider = document.querySelector(".weekendMadSlider");
+    function renderWeekendMad(recipe) {
+    console.log('recipe:', recipe);
+
+    const weekendMadSlider = document.querySelector(".weekendMadSlider");
+    const weekendMadContainer = document.createElement("div");
+    weekendMadContainer.classList.add("weekendMadContainer");
+
+    if (Array.isArray(recipe)) {
+        recipe
+        .filter(recipe =>
+            Array.isArray(recipe.acf.hovedkategori) && recipe.acf.hovedkategori.some(kat => kat.term_id == slugWeekendMad)).forEach(recipe => {
+
+            const link = document.createElement("a");
+            link.classList.add("sliderItem");
+            link.href = `./opskrift.html?slug=${recipe.slug}`;
+
+            const imgEl = document.createElement("img");
+            imgEl.classList.add("billedeISlider");
+            imgEl.src = recipe.acf.billede.url;
+            imgEl.alt = recipe.acf.billede.alt;
+
+            const title = document.createElement("span");
+            title.classList.add("recipeTitle");
+            title.textContent = recipe.title.rendered;
+
+            link.appendChild(imgEl);
+            link.appendChild(title);
+            weekendMadContainer.appendChild(link);
+        });
+    }
+
+  weekendMadSlider.appendChild(weekendMadContainer);
+}
+    const slugHurtigMad = "15"; 
+    const slugGaesteMad = "44";
+    const slugWeekendMad = "43";
    //asynkron funktion der venter på at vi har fået vores token (en form for adgangskode), ellers har vi ikke tilladelse til at hente vores private opskrifter.
-    async function fetchAndRenderRecipe() {
-        // Vi venter på tokenet inden geTRecipeById køres.
-    const token = await getToken(); // Vent på tokenet
-    if (token) {
-        await getRecipeById(token, theSlug); 
-        // Hent opskriften med tokenet og med slug eller id kan også anvendes! Har valgt med slug her. 
-    }
+ async function fetchAndRenderRecipe() {
+  const token = await getToken();
+  if (token) {
+    const options = {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    const res = await fetch(
+      domain + postsEndpoint + "?status=private&per_page=100" + getRealImageUrls,
+      options
+    );
+    const recipe = await res.json();
+ 
+    renderHurtigMad(recipe);
+    renderGaesteMad(recipe);
+    renderWeekendMad(recipe);
+  }
 }
 //kalder funktionen
     fetchAndRenderRecipe();
